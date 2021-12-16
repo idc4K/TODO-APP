c34c0cc9764231d87620e7e202fc67d12fdb90f3
christ <christikonga83@gmail.com>
2021-12-16 19:10:46 +0100

    todo

--- App/todo/views.py
+++ App/todo/views.py
@@ -6,6 +6,12 @@ from .forms import *
 def todo(request):
     q = question.objects.all()
     form = questionForm
+   
+   #Creation d'une question 
+    if request.method == 'POST':
+        form = questionForm(request.POST)
+        if form.is_valid():
+            form.save()
     context = {'q':q,'form':form}
     return render(request, 'todo/index.html', context)
 # Create your views here.
