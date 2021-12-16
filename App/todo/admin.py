d316272637ca1ec91a1746157af361b4b2746dfe
christ <christikonga83@gmail.com>
2021-12-16 18:41:05 +0100

    todo

--- App/todo/admin.py
+++ App/todo/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import question
 
+admin.site.register(question)
 # Register your models here.
--- App/todo/views.py
+++ App/todo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import *
 def todo(request):
-    return render(request, 'todo/index.html')
+    q = question.objects.all()
+    context = {'q':q}
+    return render(request, 'todo/index.html', context)
 # Create your views here.
