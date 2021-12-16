133021044e660667dae896ec940e521ec497c9f3
christ <christikonga83@gmail.com>
2021-12-16 18:30:41 +0100

    todo

--- App/todo/models.py
+++ App/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class question(models.Model):
+    titre = models.CharField(max_length=200)
+    complete = models.BooleanField(default=False)
+    creation = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titre
 # Create your models here.
