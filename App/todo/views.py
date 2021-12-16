cbed32fba42fc3fb45e56c78df2720f07a691899
christ <christikonga83@gmail.com>
2021-12-16 19:06:41 +0100

    todo

--- App/todo/views.py
+++ App/todo/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+
+from .forms import *
 def todo(request):
     q = question.objects.all()
-    context = {'q':q}
+    form = questionForm
+    context = {'q':q,'form':form}
     return render(request, 'todo/index.html', context)
 # Create your views here.
